ab71ba388f7ce9c8b762dc7d789213a42fe40241
Hannah Miller <hnmiller92@gmail.com>
2020-06-15 18:38:41 -0500

    Through 12.3.3

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,23 +16,22 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title","Create Event");
         return "events/create";
     }
 
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
-        events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:";
     }
 
